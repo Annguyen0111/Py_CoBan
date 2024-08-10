f31b7ee9a402e931b217355a48408f5c16bcad0f
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-08-10 20:22:41 +0700

    B1

---
+++ B3_Sosanh.py
@@ -0,0 +1,9 @@
+a = 1
+b = 2
+
+print(a > b)
+print(a < b)
+print(a > 1 and b > 1)
+print(a > 1 or b > 1)
+print(a > 0 & b > 0)
+print(a > 1 | b < 0)
---
+++ B4_IfElse.py
@@ -0,0 +1,7 @@
+a = input()
+a = int(a)
+
+if(a >= 0):
+    print(a)
+else:
+    print(0)
